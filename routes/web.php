a992ac1133041397891bd3952143a5e3ed4529a9
Jessica Rodriguez <jr@mettrr.com>
2017-06-21 09:27:54 +0000

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+// when you visit the homepage, all posts are loaded
+Route::get('/', 'PostsController@index');
+
+// controller => PostsController
+
+// eloquent model => Post
+
+//migration => create_posts_table
+
