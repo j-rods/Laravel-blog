3b3eb73b71cfc4236a939be886c534ead9e3698c
Jessica Rodriguez <jr@mettrr.com>
2017-06-21 10:19:42 +0000

    layout, posts controller, model & migration

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostsController extends Controller
+{
+    
+    // this is a controller action
+    public function index()
+    
+    {
+        
+        // load a view that will be in a posts folder and will have a name that corresponds to the action
+        return view('posts.index');
+        
+    }
+}
