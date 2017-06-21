b355958d439a64799bdc87a6024cc99e6dca4b38
Jessica Rodriguez <jr@mettrr.com>
2017-06-21 13:14:50 +0000

    create show posts view and set controller routing

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -6,14 +6,16 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
-    
     // this is a controller action
     public function index()
-    
     {
-        
         // load a view that will be in a posts folder and will have a name that corresponds to the action
         return view('posts.index');
-        
     }
+    
+    public function show()
+    {
+        return view('posts.show');
+    }
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,5 @@
 // when you visit the homepage, all posts are loaded
 Route::get('/', 'PostsController@index');
 
-
-
-// controller => PostsController
-
-// eloquent model => Post
-
-//migration => create_posts_table
+Route::get('/posts/{post}', 'PostsController@show');
 
