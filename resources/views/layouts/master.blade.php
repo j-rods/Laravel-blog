632e5ce4745ecfd682a11142e15b2fcdda7a2ad8
Jessica Rodriguez <jr@mettrr.com>
2017-06-21 13:57:05 +0000

    switch to blog theme from bootstrap

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -15,10 +15,41 @@
 
 
     <!-- Custom styles for this template -->
-    <link href="css/album.css" rel="stylesheet">
+    <link href="css/blog.css" rel="stylesheet">
   </head>
+  
+  <!--<head>-->
+  <!--  <meta charset="utf-8">-->
+  <!--  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">-->
+  <!--  <meta name="description" content="">-->
+  <!--  <meta name="author" content="">-->
+  <!--  <link rel="icon" href="../../favicon.ico">-->
+
+  <!--  <title>Blog Template for Bootstrap</title>-->
+
+    <!-- Bootstrap core CSS -->
+  <!--  <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">-->
+
+    <!-- Custom styles for this template -->
+  <!--  <link href="blog.css" rel="stylesheet">-->
+  <!--</head>-->
+  
+  
+
 
   <body>
+    
+    <!--<div class="blog-masthead">-->
+    <!--  <div class="container">-->
+    <!--    <nav class="nav blog-nav">-->
+    <!--      <a class="nav-link active" href="#">Home</a>-->
+    <!--      <a class="nav-link" href="#">New features</a>-->
+    <!--      <a class="nav-link" href="#">Press</a>-->
+    <!--      <a class="nav-link" href="#">New hires</a>-->
+    <!--      <a class="nav-link" href="#">About</a>-->
+    <!--    </nav>-->
+    <!--  </div>-->
+    <!--</div>-->
 
     <div class="collapse bg-inverse" id="navbarHeader">
       <div class="container">
