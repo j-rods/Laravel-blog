3743aea3c085a9c133146c628dd9b5d1961f4fcd
Jessica Rodriguez <jr@mettrr.com>
2017-06-22 10:45:02 +0000

    store a post in the database

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,23 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostsController extends Controller
 {
     // this is a controller action
-    public function index()
-    {
+    public function index() {
         // load a view that will be in a posts folder and will have a name that corresponds to the action
         return view('posts.index');
     }
     
-    public function show()
-    {
+    public function show() {
         return view('posts.show');
     }
     
     public function create() {
         return view('posts.create');
+    }
+    
+    public function store() {
+        //end execution and then dump all the request data to the page
+        // dd(request()->all()); 
+        // create a new post using the request data
+        $post = new Post;
+        $post->title = request('title');
+        $post->body = request('body');
+        // save it to the database
+        $post->save();
+        // Redirect to home page
+        return redirect('/');
     }
     
 }
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -6,6 +6,7 @@
     <h1>Publish a Post</h1>
     
     <form method="POST" action="/posts">
+      {{ csrf_field() }}
       <div class="form-group">
         <label for="title">Title</label>
         <input type="text" class="form-control" id="title" name="title">
