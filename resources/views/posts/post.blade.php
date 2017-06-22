d1794400c3983397b7a438e95d0a08d621489c23
Jessica Rodriguez <jr@mettrr.com>
2017-06-22 14:23:20 +0000

    format post datetime and create title link

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -1,6 +1,12 @@
 <div class="blog-post">
-  <h2 class="blog-post-title">{{ $post->title }}</h2>
-  <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
+  <h2 class="blog-post-title">
+    <a href="/posts/{{ $post->id }}">
+      {{ $post->title }}
+    </a>
+  </h2>
+  <p class="blog-post-meta">
+    {{ $post->created_at->toFormattedDateString() }}
+  </p>
 
   {{ $post->body }}
 </div><!-- /.blog-post -->
