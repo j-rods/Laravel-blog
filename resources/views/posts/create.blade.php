e8b9704ca1aff6926d2db2a5c47fe1714a6e074d
Jessica Rodriguez <jr@mettrr.com>
2017-06-22 13:18:50 +0000

    extract form error alert into layout partial

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -20,20 +20,7 @@
       <div class="form-group">
         <button type="submit" class="btn btn-primary">Publish</button>
       </div>
-      
-
-      
-      @if (count($errors))
-        <div class="form-group">
-          <div class="alert alert-danger">
-            <ul>
-              @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-              @endforeach
-            </ul>
-          </div>
-        </div>
-      @endif
+      @include ('layouts.errors')
     </form>
   </div>
 @endsection
