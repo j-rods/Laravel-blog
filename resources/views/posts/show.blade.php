cffdfc554cda92a507d8f69f4aaf48af65efe935
Jessica Rodriguez <jr@mettrr.com>
2017-06-22 16:02:42 +0000

    display time of post formatted to how long ago it was created

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,9 +7,11 @@
       
       <div class="comments">
         <ul class="list-group">
-         
           @foreach ($post->comments as $comment)
             <li class="list-group-item">
+              <strong>
+                {{ $comment->created_at->diffForHumans() }}: &nbsp;
+              </strong>
               {{ $comment->body }}
             </li>
           @endforeach
