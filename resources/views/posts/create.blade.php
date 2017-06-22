3fc079c86986546317facfa2415d568b72493e95
Jessica Rodriguez <jr@mettrr.com>
2017-06-22 09:52:29 +0000

    form for creating new posts

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,6 +3,21 @@
 @section ('content')
 
   <div class="col-sm-8 blog-main">
-    <h1>Create a Post</h1>
+    <h1>Publish a Post</h1>
+    
+    <form method="POST" action="/posts">
+      <div class="form-group">
+        <label for="title">Title</label>
+        <input type="text" class="form-control" id="title" name="title">
+      </div>
+      
+      <div class="form-group">
+        <label for="body">Body</label>
+        <textarea class="form-control" id="body" name="body"></textarea>
+      </div>
+      
+      <button type="submit" class="btn btn-primary">Publish</button>
+    </form>
+    
   </div>
 @endsection
