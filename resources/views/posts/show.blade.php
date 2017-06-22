0439a10d1ecd7344921ec5cb680620ce206cfe95
Jessica Rodriguez <jr@mettrr.com>
2017-06-22 15:54:21 +0000

    display comments under a post

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,5 +4,16 @@
   <div class="col-sm-8 blog-main">
       <h2>{{ $post->title }}</h2>
       <p> {{ $post->body }}</p>
+      
+      <div class="comments">
+        <ul class="list-group">
+         
+          @foreach ($post->comments as $comment)
+            <li class="list-group-item">
+              {{ $comment->body }}
+            </li>
+          @endforeach
+        </ul>
+      </div>
   </div>
 @endsection
