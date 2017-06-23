e73762f716d87f4333f490c6dde948162aedc80a
Jessica Rodriguez <jr@mettrr.com>
2017-06-23 14:55:40 +0000

    user logout

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 class SessionsController extends Controller
 {
     public function create() {
-        
+       
+    }
+    public function destroy() {
+      auth()->logout();
+      return redirect()->home();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -9,4 +9,5 @@ Route::post('/posts/{post}/comments', 'CommentsController@store');
 Route::get('/register', 'RegistrationController@create');
 Route::post('/register', 'RegistrationController@store');
 Route::get('/login', 'SessionsController@create');
+Route::get('/logout', 'SessionsController@destroy');
 
