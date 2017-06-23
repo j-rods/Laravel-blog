2d735fe36c1ad9bba6de6e6e4e0ac2076264cf08
Jessica Rodriguez <jr@mettrr.com>
2017-06-23 14:40:37 +0000

    register new user in browser

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -2,11 +2,25 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\User;
 
 class RegistrationController extends Controller
 {
     public function create() {
         return view('sessions.create');
     }
+    public function store() {
+      // validate form
+      $this->validate(request(), [
+        'name' => 'required',
+        'email' => 'required',
+        'password' => 'required'
+      ]);
+       // create and save the user
+       $user = User::create(request(['name', 'email', 'password']));
+       // sign them in
+       auth()->login($user);
+       // redirect to the home page
+       return redirect()->home();
+    } 
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 // when you visit the homepage, all posts are loaded
-Route::get('/', 'PostsController@index');
+Route::get('/', 'PostsController@index')->name('home');
 Route::get('/posts/create', 'PostsController@create');
 Route::post('/posts', 'PostsController@store');
 Route::get('/posts/{post}', 'PostsController@show');
