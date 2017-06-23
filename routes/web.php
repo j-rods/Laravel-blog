5a3ea71dca98b3b707ddc9e11e1ab65e5692ec1c
Jessica Rodriguez <jr@mettrr.com>
2017-06-23 08:52:08 +0000

    create comments in the browser

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,9 @@
 
 // when you visit the homepage, all posts are loaded
 Route::get('/', 'PostsController@index');
-
 Route::get('/posts/create', 'PostsController@create');
-
 Route::post('/posts', 'PostsController@store');
-
 Route::get('/posts/{post}', 'PostsController@show');
+Route::post('/posts/{post}/comments', 'CommentsController@store');
+
 
