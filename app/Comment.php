d1f7f126e5e0bc1cb65e07253e1beb266966b41f
Jessica Rodriguez <jr@mettrr.com>
2017-06-23 12:35:09 +0000

    implement user relationship

--- app/Comment.php
+++ app/Comment.php
@@ -7,4 +7,7 @@ class Comment extends Model
   public function post() {
      return $this->belongsTo(Post::class);
   }
+  public function user() { // $comment->user->name
+     return $this->belongsTo(User::class);
+  }
 }
