a3b702e71d857f6048c8e4d270e711449a865918
Jessica Rodriguez <jr@mettrr.com>
2017-06-23 14:11:13 +0000

    sessions form

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -3,5 +3,28 @@
 @section ('content')
   <div class="col-sm-8 blog-main">
       <h1>Register</h1>
+      
+      <form method="POST" action="/register">
+          {{ csrf_field() }}
+          
+          <div class="form-group">
+              <label for="name">Name:</label>
+              <input type="text" class="form-control" id="name" name="name">
+          </div>
+          
+          <div class="form-group">
+              <label for="email">Email:</label>
+              <input type="email" class="form-control" id="email" name="email">
+          </div>
+          
+          <div class="form-group">
+              <label for="password">Password:</label>
+              <input type="text" class="form-control" id="password" name="password">
+          </div>
+          
+          <div class="form-group">
+              <button type="submit" class="btn btn-primary">Register</button>
+          </div>
+      </form>
   </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,6 @@ Route::post('/posts', 'PostsController@store');
 Route::get('/posts/{post}', 'PostsController@show');
 Route::post('/posts/{post}/comments', 'CommentsController@store');
 Route::get('/register', 'RegistrationController@create');
+Route::post('/register', 'RegistrationController@store');
 Route::get('/login', 'SessionsController@create');
 
