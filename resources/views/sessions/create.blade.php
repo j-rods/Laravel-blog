a9b23966fd3379cb76d0b03a93bfe3370ff91081
Jessica Rodriguez <jr@mettrr.com>
2017-06-26 08:53:27 +0000

    login form

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -3,5 +3,25 @@
 @section ('content')
     <div class="col-sm-8 blog-main">
       <h1>Sign In</h1>
+      
+      <form method="POST" action="/sessions">
+        {{ csrf_field() }}
+        
+        <div class="form-group">
+          <label for="email">Email Address:</label>
+          <input type="email" class="form-control" id="email" name="email">
+        </div>
+        
+        <div class="form-group">
+          <label for="passord">Password:</label>
+          <input type="password" class="form-control" id="password" name="password">
+        </div>
+        
+        <div class="form-group">
+          <button type="submit" class="btn btn-primary">Sign In</button>
+        </div>
+        
+      </form>
+      
     </div>
 @endsection
