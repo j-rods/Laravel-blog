231682050a439395efb01275cc675348aace739e
Jessica Rodriguez <jr@mettrr.com>
2017-06-27 12:17:40 +0000

    register a secret key

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,19 @@
 <?php
 
+// Bind into the container
+// Allows to register any key into the container
+// and then associate that with some value
+App::bind('App\Billing\Stripe', function () {
+    // return a new instance of stripe class
+    return new \App\Billing\Stripe(config('services.stripe.secret')); // pass through a secret key
+});
+
+// resolve that instance out of the container
+$stripe = App::make('App\Billing\Stripe');
+
+dd($stripe);
+
+
 // when you visit the homepage, all posts are loaded
 Route::get('/', 'PostsController@index')->name('home');
 Route::get('/posts/create', 'PostsController@create');
