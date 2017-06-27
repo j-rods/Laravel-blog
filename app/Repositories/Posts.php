2b34f29bf3580cbe2fde53161e53e7aa11fd70c8
Jessica Rodriguez <jr@mettrr.com>
2017-06-27 10:43:09 +0000

    class dependency

--- app/Repositories/Posts.php
+++ app/Repositories/Posts.php
@@ -3,9 +3,22 @@
 namespace App\Repositories;
 
 use App\Post;
+use App\Redis;
 
 class Posts {
-  //this can be a collection of sorts
+  // this can be a collection of sorts
+  
+  // what if a class needs to depend upon another class
+  // in order for posts to do its job, what do we need
+  // create a Redis class
+  
+  // protected property
+  protected $redis;
+  
+  public function __construct(Redis $redis) {
+    $this->redis = $redis;
+  } 
+  
   public function all() {
       // return all relevant posts
       return Post::all();
