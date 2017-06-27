698349e2d7a881f525f6ef80b46389cb3b3b0113
Jessica Rodriguez <jr@mettrr.com>
2017-06-27 09:06:18 +0000

    refactor unit test assertion for creating posts

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -29,8 +29,18 @@ class ExampleTest extends TestCase
         
         // When I fetch the archives.
         $posts = Post::archives();
-        dd($posts);
         // Then the response should be in the proper format.
-        $this->assertCount(2, $posts);
+        $this->assertEquals([
+            [
+            "year" => $first->created_at->format('Y'),
+            "month" => $first->created_at->format('F'),
+            "published" => 1          
+            ],
+            [
+            "year" => $second->created_at->format('Y'),
+            "month" => $second->created_at->format('F'),
+            "published" => 1          
+            ]
+        ], $posts);
     }
 }
