dc919599ac8bba58a35065b2689511ca9faca863
Jessica Rodriguez <jr@mettrr.com>
2017-06-27 08:59:13 +0000

    unit test for creating posts

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,9 +2,11 @@
 
 namespace Tests\Unit;
 
+use App\Post;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+
 
 class ExampleTest extends TestCase
 {
@@ -13,8 +15,22 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
+    use DatabaseTransactions;
+    
     public function testBasicTest()
     {
-        $this->assertTrue(true);
+        // Given I have 2 records in the database that are posts,
+        // and each one is posted a month apart.
+        $first = factory(Post::class)->create();
+        
+        $second = factory(Post::class)->create([
+          'created_at' => \Carbon\Carbon::now()->subMonth()
+        ]);
+        
+        // When I fetch the archives.
+        $posts = Post::archives();
+        dd($posts);
+        // Then the response should be in the proper format.
+        $this->assertCount(2, $posts);
     }
 }
