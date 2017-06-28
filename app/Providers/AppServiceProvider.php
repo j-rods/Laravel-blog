0789849a83e97e34e3f511b05336f722a7c74496
Jessica Rodriguez <jr@mettrr.com>
2017-06-28 14:06:11 +0000

    refactor view archives and tags in app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,8 +18,9 @@ class AppServiceProvider extends ServiceProvider
         Schema::defaultStringLength(191);
         
         view()->composer('layouts.sidebar', function ($view) {
-            $view->with('archives', \App\Post::archives());
-            $view->with('tags', \App\Tag::has('posts')->pluck('name'));
+            $archives = \App\Post::archives();
+            $tags = \App\Tag::has('posts')->pluck('name');
+            $view->with(compact('archives', 'tags'));
         });
     }
 
